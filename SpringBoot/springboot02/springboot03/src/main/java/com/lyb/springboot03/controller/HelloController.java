cc9494d30acf0ee704c3b8e3b01a8a8b929e0b75
LiYB <694707966@qq.com>
2020-09-04 00:59:00 +0800

    9.03

---
+++ SpringBoot/springboot02/springboot03/src/main/java/com/lyb/springboot03/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.lyb.springboot03.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello boot";
+    }
+}
