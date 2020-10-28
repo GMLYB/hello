2d1283d959c46dedfaa980607800cd2a45bca9dd
LiYB <694707966@qq.com>
2020-10-29 00:07:47 +0800

    10.28

---
+++ LeetCode/202010/LeetCode_258_1028.java
@@ -0,0 +1,32 @@
+package October;
+
+import org.junit.Test;
+/*
+* 给定一个非负整数 num，反复将各个位上的数字相加，直到结果为一位数。
+
+示例:
+输入: 38
+输出: 2
+解释: 各位相加的过程为：3 + 8 = 11, 1 + 1 = 2。 由于 2 是一位数，所以返回 2。
+
+* */
+
+public class LeetCode_258 {
+    public int addDigits(int num) {
+        int sum = 0;
+        while (num >= 10) {
+            while (num != 0) {
+                sum += num % 10;
+                num = num / 10;
+            }
+            num = sum;
+            sum = 0;
+        }
+        return num;
+    }
+
+    @Test
+    public void test(){
+        System.out.println(addDigits(10));
+    }
+}
