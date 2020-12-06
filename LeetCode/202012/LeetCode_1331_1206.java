262785294214de446bd749a9363153550f538c24
lyb <694707966@qq.com>
2020-12-06 22:51:46 +0800

    12.06

---
+++ LeetCode/202012/LeetCode_1331_1206.java
@@ -0,0 +1,68 @@
+package December;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+
+/*
+1331. 数组序号转换
+
+给你一个整数数组 arr ，请你将数组中的每个元素替换为它们排序后的序号。
+序号代表了一个元素有多大。序号编号的规则如下：
+序号从 1 开始编号。
+一个元素越大，那么序号越大。如果两个元素相等，那么它们的序号相同。
+每个数字的序号都应该尽可能地小。
+ 
+
+示例 1：
+输入：arr = [40,10,20,30]
+输出：[4,1,2,3]
+解释：40 是最大的元素。 10 是最小的元素。 20 是第二小的数字。 30 是第三小的数字。
+
+示例 2：
+输入：arr = [100,100,100]
+输出：[1,1,1]
+解释：所有元素有相同的序号。
+
+示例 3：
+输入：arr = [37,12,28,9,100,56,80,5,12]
+输出：[5,3,4,2,8,6,7,1,3]
+ 
+提示：
+0 <= arr.length <= 105
+-109 <= arr[i] <= 109
+
+ */
+public class LeetCode_1331 {
+
+	public int[] arrayRankTransform(int[] arr) {
+		int[] sortnum = new int[arr.length];
+		int[] clarr = arr.clone();
+		Arrays.sort(clarr);
+		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+		int pm = 1;
+		for (int i = 0; i < clarr.length; i++) {
+			if (!map.containsKey(clarr[i])) {
+				map.put(clarr[i], pm++);
+			}
+
+		}
+
+		for (int i = 0; i < arr.length; i++) {
+			sortnum[i] = map.get(arr[i]);
+		}
+
+		return sortnum;
+	}
+
+	@Test
+	public void test() {
+		int[] nums1 = new int[] { 40, 10, 20, 30 };
+		int[] res = arrayRankTransform(nums1);
+		for (int i = 0; i < res.length; i++) {
+			System.out.println(res[i]);
+		}
+	}
+}
